5b04601a54b017900b7f011261304575bf85949a
Deepak M S <souparnikad40@gmail.com>
2021-10-27 17:53:32 +0530

    ui and firestor plugin added

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:board_app/ui/board_app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,59 +13,10 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        elevation: 0,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      home: const BoardApp(),
     );
   }
 }
---
+++ lib/ui/board_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BoardApp extends StatefulWidget {
+  const BoardApp({Key? key}) : super(key: key);
+
+  @override
+  _BoardAppState createState() => _BoardAppState();
+}
+
+class _BoardAppState extends State<BoardApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
