8e0ef45abb2b17dd0ae43de04ddff9b0c3db6e6e
Deepak M S <souparnikad40@gmail.com>
2021-10-27 18:20:42 +0530

    firestor issue solved

--- lib/ui/board_app.dart
+++ lib/ui/board_app.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class BoardApp extends StatefulWidget {
@@ -8,8 +11,26 @@ class BoardApp extends StatefulWidget {
 }
 
 class _BoardAppState extends State<BoardApp> {
+  var firestoreDB = FirebaseFirestore.instance.collection("board").snapshots();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Firbase Firestore"),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance.collection('board').snapshots(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return const CircularProgressIndicator();
+          }
+          return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (context, int index) {
+                return Text(snapshot.data!.docs[index]['title']);
+              });
+        },
+      ),
+    );
   }
 }
